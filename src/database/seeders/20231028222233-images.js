693239a8772be5e0188b777d8aa51ebfc48e18f6
shezedeth@gmail.com <sheze.gomez@gmail.com>
2023-11-09 12:05:06 -0300

    migracion y seed funcionando

--- src/database/seeders/20231028222233-images.js
+++ src/database/seeders/20231028222233-images.js
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert(
-      "Products",
+      "Images",
       [
         // Adidas - Superstar ----------------------------------
         {
@@ -701,6 +701,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Products", null, {});
+    await queryInterface.bulkDelete("Images", null, {});
   },
 };
